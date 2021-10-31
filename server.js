b51e981dc3ea25821061cc9123c420acc0dec85c
Ghidorah <jljohnson1454@gmail.com>
2021-10-31 10:27:16 -0500

    can view everything

--- server.js
+++ server.js
@@ -91,18 +91,65 @@ const employeeDatabase = () => {
     };
 
     showDepartments = () => {
-        const query = (`SELECT department.id AS id, department.name AS department FROM department`);
+        // const query = (`SELECT department.id AS id, department.name AS department FROM department`);
         
         db.query(`SELECT * FROM departments`, (err,rows) => {
             console.table(rows);
+            employeeDatabase();
         })
-        console.log('POOP');
     }
 
     showRoles = () => {
-        const query = (`SELECT roles.id AS id, roles.title AS title, roles.salary AS salary FROM roles`);
+        // const query = (`SELECT roles.id AS id, roles.title AS title, roles.salary AS salary FROM roles`);
 
         db.query(`SELECT * FROM roles`, (err, rows) => {
             console.table(rows);
+            employeeDatabase();
         })
+        
         }
+
+    showEmployees = () => {
+        //const query = (`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary`)
+        db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.roles_id, employee.manager_id FROM employee`, (err, rows) => {
+            console.table(rows);
+            employeeDatabase();
+        })
+    }
+
+    addDepartment = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'addDepartment',
+                message: 'Please enter a new department'
+            }
+        ])
+        .then(answer => {
+            db.query(`INSERT INTO departments (name) VALUES (?)`, answer.addDepartment, (err, result) => {
+                console.log('Department ' + answer.addDepartment + ' has been added!')
+                showDepartments();
+            })
+        })
+    }
+
+    addRole = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'addRole',
+                message: 'Please enter a new role!'
+            }
+            // {
+            //     type: 'input',
+            //     name: 'roleSalary',
+            //     message: 'What is the salary of this role?'
+            // }
+        ])
+        .then(answer => {
+            db.query(`INSERT INTO roles (title) VALUES (?)`, answer.addRole, (err, result) => {
+                console.log('Role ' + answer.addRole + ' has been added!')
+                showRoles();
+            })
+        })
+    }
